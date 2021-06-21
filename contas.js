712da2e8f6efc048f4b460aea1964ad8df0e6383
Adriano-Ivan <adrianoivandepaulacampos@gmail.com.br>
2021-06-21 08:37:58 -0300

    Implementando funções de exibir movimentos, exibir balanço, e exibir sumário.

---
+++ contas.js
@@ -0,0 +1,26 @@
+const account1 = {
+  owner: "Joe Tolbad",
+  movements: [2000, 29, -234, 400, -23, -80, 295, 23],
+  interestRate: 1.2,
+  pin: 1111,
+};
+const account2 = {
+  owner: "Amanda Jellis",
+  movements: [23, -8, 250, 35, -29, -76, 245, 67, 29],
+  interestRate: 1.2,
+  pin: 2222,
+};
+const account3 = {
+  owner: "Julius Soray",
+  movements: [560, -78, 23, 45, 21, -6, 79, 20],
+  interestRate: 0.9,
+  pin: 3333,
+};
+const account4 = {
+  owner: "Adriano Ivan",
+  movements: [235, 580, -25, 26, 76, 24, -8, -50, -12],
+  interestRate: 1.4,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
--- script.js
+++ script.js
@@ -9,10 +9,12 @@ const sortValuesButton = document.querySelector(".sort-values-button");
 
 // Elements
 const movementsArea = document.querySelector(".movements");
-const totalDeposits = document.querySelector(".deposits-total");
-const totalWithdrawals = document.querySelector(".withdrawals-total");
-const totalInterests = document.querySelector(".interest-total");
+const totalDeposits = document.querySelector(".deposits-total span");
+const totalWithdrawals = document.querySelector(".withdrawals-total span");
+const totalInterest = document.querySelector(".interest-total span");
 const balanceArea = document.querySelector(".balance");
+const welcomeArea = document.querySelector(".welcome");
+const valueBalanceArea = document.querySelector(".value-balance");
 
 // Inputs
 const loginInput = document.querySelector(".username");
@@ -23,3 +25,53 @@ const loanUserInput = document.querySelector("#username-loan");
 const valueLoanInput = document.querySelector("#value-money-loan");
 const closeUserInput = document.querySelector("#username-now");
 const closePinUserInput = document.querySelector("#pin-user");
+
+// Functions
+const displayMovements = function (movs) {
+  movementsArea.innerHTML = "";
+
+  movs.forEach(function (mov, i) {
+    const type = mov > 0 ? "deposit" : "withdrawal";
+
+    const html = `<div class="movement">
+    <div class="${type}_date">
+      <div class="${type}">${i + 1} ${type}</div>
+      <div class="date">17/05/2020</div>
+    </div>
+    <div class="value">R$${mov}</div>
+  </div>`;
+
+    movementsArea.insertAdjacentHTML("afterbegin", html);
+  });
+};
+
+const calcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((ac, mov) => ac + mov, 0);
+  valueBalanceArea.textContent = `R$ ${acc.balance}`;
+};
+
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
+    .filter((mov) => mov > 0)
+    .reduce((ac, mov) => ac + mov, 0);
+  totalDeposits.textContent = `R$ ${incomes}`;
+
+  const out = acc.movements
+    .filter((mov) => mov < 0)
+    .reduce((ac, mov) => ac + mov, 0);
+  totalWithdrawals.textContent = `R$ ${Math.abs(out)}`;
+
+  const interest = acc.movements
+    .filter((mov) => mov > 0)
+    .map((deposit) => (deposit * acc.interestRate) / 100)
+    .filter((interest, i, arr) => {
+      //console.log(arr);
+      return interest >= 1;
+    })
+    .reduce((ac, interest) => ac + interest, 0);
+  totalInterest.textContent = `R$ ${interest}`;
+};
+
+displayMovements(account1.movements);
+calcDisplayBalance(account1);
+calcDisplaySummary(account1);
